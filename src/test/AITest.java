77fd308c4794edbfeadc0f4355959a0f08c076b9
Adam Fox <adamfox26@gmail.com>
2017-03-30 15:39:43 +0100

    Added game graph & AITest.java

---
+++ src/test/AITest.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by adam on 28/03/2017.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        shortestDistTest.class
+})
+public class AITest {
+}
