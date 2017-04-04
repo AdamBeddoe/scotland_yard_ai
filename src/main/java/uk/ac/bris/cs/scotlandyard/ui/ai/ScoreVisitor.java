f4d0692e078e45fd6c5db71b931062a4185275a7
Adam Beddoe <adam.beddoe@gmail.com>
2017-04-04 16:15:40 +0100

    printfs

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ScoreVisitor.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ScoreVisitor.java
@@ -16,20 +16,20 @@ public class ScoreVisitor extends TreeVisitor {
         if (tree.isMrXRound()) {
             int bestScore = Integer.MIN_VALUE;
             for (GameTree childTree : tree.getChildTrees()) {
-                visit(childTree); // vs childTree.accept(this) should be consistent with other visitors
+                if (!childTree.isDeadNode()) visit(childTree); // vs childTree.accept(this) should be consistent with other visitors
                 if (childTree.getScore() > bestScore) {
                     tree.setScore(childTree.getScore());
                     bestScore = childTree.getScore();
                 }
             }
-
+            if (tree.getChildTrees().isEmpty()) System.out.println("Assigning score " + this.ai.scoreBoard(tree.getState()));
             if (tree.getChildTrees().isEmpty()) tree.setScore(this.ai.scoreBoard(tree.getState()));
         }
 
         else {
             int worstScore = Integer.MAX_VALUE;
             for (GameTree childTree : tree.getChildTrees()) {
-                visit(childTree);
+                if (!childTree.isDeadNode()) visit(childTree);
                 if (childTree.getScore() < worstScore) {
                     tree.setScore(childTree.getScore());
                     worstScore = childTree.getScore();
@@ -37,5 +37,6 @@ public class ScoreVisitor extends TreeVisitor {
             }
             if (tree.getChildTrees().isEmpty()) tree.setScore(this.ai.scoreBoard(tree.getState()));
         }
+        System.out.println("                      Visiting");
     }
 }
